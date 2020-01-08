fae15ebd774f830fa2547935723b5d5a6bc297cf
Gabriel Lima <s5006074@bournemouth.ac.uk>
2020-01-08 18:33:31 +0000

    Camera and transform is working and need to add textures and mesh and work on resource

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -8,15 +8,30 @@ int main()
 	std::cout << "Hello Game" << std::endl;
 	std::shared_ptr<Application> application = Application::initialize();
 	
-	std::shared_ptr<Entity> gameObject = application->addEntity();
+	std::shared_ptr<Entity> triangleObject = application->addEntity();
+	std::shared_ptr<Entity> currentCamera = application->addEntity();
 
-	std::shared_ptr<Meshrenderer> triangle = gameObject->addComponent<Meshrenderer>();
-	std::shared_ptr<Transform> trTransform = gameObject->addComponent<Transform>();
-	std::shared_ptr<Camera> camera = gameObject->addComponent<Camera>();
+	std::shared_ptr<Meshrenderer> triangle = triangleObject->addComponent<Meshrenderer>();
+	std::shared_ptr<Transform> trTransform = triangleObject->addComponent<Transform>();
 
-	trTransform->setLocalpos(glm::vec3(0, 0, 0));
-	trTransform->setLocalrot(glm::vec3(0, 0, 0));
-	trTransform->setLocalScale(glm::vec3(1.0f));
+
+
+	std::shared_ptr<Camera> camera = currentCamera->addComponent<Camera>();
+	std::shared_ptr<Transform> camTransform = currentCamera->addComponent <Transform>();
+
+	triangleObject->getComponent<Transform>()->setLocalpos(glm::vec3(0, 0, 0));
+	triangleObject->getComponent<Transform>()->setLocalrot(glm::vec3(0, 0, 0));
+	triangleObject->getComponent<Transform>()->setLocalScale(glm::vec3(1.0f, 1.0f, 1.0f));
+	//gameObject->getComponent<Transform>()->setLocalScale(glm::vec3(1.0f));
+
+	currentCamera->getComponent<Transform>()->setLocalpos(glm::vec3(0, 0, 5));
+	currentCamera->getComponent<Transform>()->setLocalrot(glm::vec3(0, 0, 0));
+	currentCamera->getComponent<Transform>()->setLocalScale(glm::vec3(1.0f, 1.0f, 1.0f));
+	
+
+//	trTransform->setLocalpos(glm::vec3(0, 0, -5));
+//	trTransform->setLocalrot(glm::vec3(0, 0, 0));
+//	trTransform->setLocalScale(glm::vec3(1.0f));
 
 	application->start();
 
